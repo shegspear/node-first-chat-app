de38f24ec2d4e0f1122eb3ee7fb80055b025bba8
shegspear <57143805+shegspear@users.noreply.github.com>
2020-09-27 18:56:18 +0100

    Add newMessage ad create message event

--- server/server.js
+++ server/server.js
@@ -14,7 +14,27 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.on('disconnect', (socket) => {
+    socket.emit('newEmail', {
+        from: 'mike@example.com',
+        text: 'Hey. What is going on.',
+        createdAt: 123
+    });
+
+    socket.emit('newMessage', {
+        from: 'thisguy1@example.com',
+        text: 'Sup, hope your good',
+        createdAt: 123
+    });
+
+    socket.on('createEmail', (newEmail) => {
+        console.log('createEmail', newEmail);
+    });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('createMessage', newMessage);
+    });
+
+    socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
 });
